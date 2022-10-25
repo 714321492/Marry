23ba2dbcfa206105647944921dacc205cff11136
714321492 <84560231+714321492@users.noreply.github.com>
2022-10-25 14:26:08 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
+module.exports = defineConfig({
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "windows.jQuery": "jquery",
+      }),
+    ],
+  },
+  devServer: {
+    port: process.env.port
+  },
+  transpileDependencies: true,
+});
